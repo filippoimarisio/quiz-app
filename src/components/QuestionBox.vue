112f8b0a6a3100e1f7a4c34bbaf2901309130d97
filippoimarisio <filippo.imarisio@gmail.com>
2019-06-10 19:38:11 +0200

    fetches and renders questions and answers

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -7,10 +7,16 @@
       </template>
 
       <hr class="my-4">
-
-      <p v-for="(answer, index) in answers" :key="index">
-        {{ answer }}
-      </p>
+      <b-list-group>
+        <b-list-group-item
+          v-for="(answer, index) in answers" 
+          :key="index"
+          @click="selectAnswer(index)"
+          :class="[selectedIndex === index ? 'selected' : '']"
+          >
+            {{ answer }}
+        </b-list-group-item>
+      </b-list-group>
 
       <b-button variant="primary" href="#">Submit</b-button>
       <b-button @click="next" variant="success" href="#">Next</b-button>
@@ -24,12 +30,49 @@ export default {
     currentQuestion: Object,
     next: Function
   },
+  data() {
+    return {
+      selectedIndex: null
+    }
+  },
   computed: {
     answers() {
       let answers = [...this.currentQuestion.incorrect_answers]
-      answers.push(this.currentQuestion.correct_answers)
+      answers.push(this.currentQuestion.correct_answer)
       return answers
+    }
+  },
+  methods: {
+    selectAnswer(index) {
+      console.log(index)
+      this.selectedIndex = index
     }
   }
 }
 </script>
+
+<style scoped>
+  .list-group {
+    margin-bottom: 15px;
+  }
+  .list-group-item:hover {
+    background-color: #EEE;
+    cursor: pointer
+  }
+  .btn {
+    margin: 0 5px;
+  }
+
+  .selected {
+    background-color: lightblue;
+  }
+
+  .correct {
+    background-color: lightgreen;
+  }
+
+  .incorrect {
+    background-color: red;
+  }
+</style>
+
